c1adf2d0c33569bdd20ea2e41e024b11d703821d
mukhen rawling <rawling_m@qwasar.io>
2023-01-14 16:21:23 +0000

    makefile

--- my_ngram.c
+++ my_ngram.c
@@ -1,44 +1,41 @@
-# include <stdio.h>
-# include <string.h>
-# include "my_ngram.h"
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_ARRAY_SIZE 128
 
-// Macro definition and it will be used to define the size of the array "count_arr"
-#define NUM_CHARS 512
+void fill_array(int* array, char* str) {
+    int index = 0;
 
-int count_arr[NUM_CHARS];
-
-// The function check_str takes a string as input and counts the number of occurrences of each character in the string
-// Inside the loop, the current character str[i] is converted to ASCII by casting it to an int and is used as the index to access 
-// the corresponding element in the count_arr array. The value at that element is incremented by 1 for each
-// occurrences of that character in the str argument
-
-void check_str(const char *str) {
-    for(size_t i =0; i< strlen(str); i++){
-        count_arr[(int)str[i]]++;
+    while (str[index] != '\0') {
+        if (str[index] != '"') {
+            array[(int)str[index]] += 1;
+        }
+        index += 1;
     }
 }
 
-// The function num_occurrences prints the number of occurrences of each character in the str that has been counted in count_arr array
-// Inside the loop, if the value in the count_arr at current index i is grater than 0, then the corresponding character is obtained 
-// by casting "i" to a char and passed as the first argument to the printf() function. The second argument is the value at that index
-// in the count_arr array which represents the count of the character
-
-void num_occurrences(){
-    for (int i = 0; i < NUM_CHARS; i++) {
-        if (count_arr[i] > 0) {
-            printf("%c:%d\n", (char)i, count_arr[i]);
-        } 
+void print_array(int* array, int size_array) {
+    int index = 0;
+        //int print arrray
+    while (index < size_array) {
+        if (array[index] > 0) {
+            //funcion printf 
+            //-> int
+            //parametrs: const restrict-
+            printf("%c:%d\n", index, array[index]);
+        }
+        //variable index Type:int value=0
+        index += 1;
     }
 }
 
-// The main function takes command line arguments and processes them one by one. For each argument, the function "check_str" is called to count the characters and 
-// then calls "num_occurrences" function to print the final counts of number of occurrences.
+int main(int ac, char **av) {
+    int index = 1;
+    int array[MAX_ARRAY_SIZE] = {0};
 
-int main (int argc, char *argv[]) {
-    for (int i = 1; i < argc; i++) {
-        check_str(argv[i]);
+    while (index < ac) {
+        fill_array(&array[0], av[index]);
+        index += 1;
     }
-    num_occurrences();
-    return 0;
+    print_array(&array[0], MAX_ARRAY_SIZE);
+    return EXIT_SUCCESS;
 }
-
